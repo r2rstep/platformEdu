7cbc3b94842a7e5f90d6e754c038faab2d57d628
r2r <aart.st@gmail.com>
2020-12-16 18:45:07 +0100

    fix celery app

--- app/app/core/celery_app.py
+++ app/app/core/celery_app.py
@@ -5,4 +5,3 @@ from app.core.config import settings
 celery_app = Celery("worker", broker=f"amqp://guest@{settings.QUEUE_HOSTNAME}//")
 
 celery_app.conf.task_routes = {"app.tasks.batch_upload.*": "batch-upload"}
-celery_app.conf.task_track_started
