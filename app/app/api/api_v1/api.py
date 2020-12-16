d664a91ba0d8acfbf2dda33cb5117193c4a5dc5d
artur_stepniak <artur.stepniak@weartech.solutions>
2020-12-16 14:52:36 +0100

    add endpoint for returning job status

--- app/app/api/api_v1/api.py
+++ app/app/api/api_v1/api.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter
 
-from app.api.api_v1.endpoints import lectures, users, login, uploads
+from app.api.api_v1.endpoints import lectures, users, login, uploads, jobs
 
 api_router = APIRouter()
 api_router.include_router(users.router, prefix="/users", tags=["users"])
 api_router.include_router(lectures.router, prefix="/lectures", tags=["lectures"])
 api_router.include_router(login.router, tags=["login"])
 api_router.include_router(uploads.router, tags=["upload"])
+api_router.include_router(jobs.router, prefix="/jobs", tags=["jobs"])
--- app/app/api/api_v1/endpoints/jobs.py
+++ app/app/api/api_v1/endpoints/jobs.py
@@ -1,11 +1,16 @@
-from fastapi import APIRouter, Response, status
+from fastapi import APIRouter, Depends, Request
+from sqlalchemy.orm import Session
 
-from app.core.config import settings
-from app.tasks.batch_upload import process_batch_upload
+from app.api import deps
+from app import crud
+from app.logic.jobs import get_job_result
+from app.schemas.jobs import Job, JobLinks
 
 router = APIRouter()
 
 
-@router.get('/{job_id')
-def get_job_details(job_id: int):
-    pass
+@router.get('/{job_id}', response_model=Job)
+def get_job_details(job_id: int, req: Request, db: Session = Depends(deps.get_db)):
+    job_in_db = crud.job.get(db, id=job_id)
+    state, result = get_job_result(db, job_id)
+    return Job(**job_in_db, state=state, result=result, links=JobLinks(self=req.url.path.rstrip('/')))
